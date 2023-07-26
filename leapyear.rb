a846b5c0cf24c8f6ed92c6ee53e91019a61abcee
Amani T <136659501+amanitristan@users.noreply.github.com>
2023-07-26 21:19:17 +0000

    Tried a while nested in an if statement

--- leapyear.rb
+++ leapyear.rb
@@ -18,14 +18,26 @@ years = [
   1940,
   2038
 ]
+
 year = years.sample
 leap_year = "#{year} is a leap year!"
 no_leap_year = "#{year} is not a leap year."
 
-if year/4 /= year%4.0
-  pp no_leap_year
-elsif year/100 /= year%100.0
+=begin
+if year/4 == year%4.0
   pp leap_year
-else year/400 /= year%400.0
+elsif year/100 == year%100.0
+  pp no_leap_year
+else year/400 == year%400.0
+  pp leap_year
+end
+=end
+
+if year/4 == year%4
+  while year/100 == year%100
+    year/400 == year%400
+    pp leap_year
+  end
+else
   pp no_leap_year
 end
