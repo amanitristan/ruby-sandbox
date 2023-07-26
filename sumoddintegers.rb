4d292200072aee08b35046fca02d46fc71e2b95a
Amani T <136659501+amanitristan@users.noreply.github.com>
2023-07-26 21:55:23 +0000

    Added sample inputs from assignment

--- sumoddintegers.rb
+++ sumoddintegers.rb
@@ -5,3 +5,10 @@ The program should then print the sum of the odd numbers only.
 
 =end
 
+inputs = [
+  ["9", "5", "4"],
+  ["20", "40", "60"],
+  ["1", "3", "19"]
+]
+numbers = inputs.sample
+pp numbers
