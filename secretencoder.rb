2edcf710bc58ba1409e5dd000d99f59c1114361d
Amani T <136659501+amanitristan@users.noreply.github.com>
2023-07-26 21:26:13 +0000

    Added instructions

---
+++ secretencoder.rb
@@ -0,0 +1,9 @@
+=begin INSTRUCTIONS
+
+Write a program that ingests a secret message and “encodes” the message by replacing vowels with other characters
+
+Here is our secret code, don’t tell anyone: a = 1, e = 2, i = 3, o = 4, u = 5
+
+Your program should print the encoded message.
+
+=END
