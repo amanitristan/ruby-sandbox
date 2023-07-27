09b0e870c7f86a3d2c63ef679466ec70ff1b047c
Amani T <136659501+amanitristan@users.noreply.github.com>
2023-07-27 21:15:29 +0000

    Added instructions

---
+++ charactertypes.rb
@@ -0,0 +1,13 @@
+=begin INSTRUCTIONS
+
+Write a program that:
+  Takes a String and prints out the below information
+  Counts the total number of letters in the given String
+  Counts the total number of spaces in the given String
+  Counts the total numbers of digits in the given String
+Example output for string = "here 12 plus 25":
+  "Number of letters in the string is: 8"
+  "Number of spaces in the string is: 3"
+  "Number of digits in the string is: 4"
+
+=end
